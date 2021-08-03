eb2c65307aaefcd8b1fc8009024b05fab23ae7af
bearbearbear <bear-bear-bear@kakao.com>
2021-08-03 23:58:11 +0900

    feat: Create Product 테스트 작성

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,8 +1,5 @@
-describe('Example test', () => {
-  test('two plus two is four', () => {
-    expect(2+2).toBe(4);
-  });
-  test('two plus two is not five', () => {
-    expect(2+2).not.toBe(5);
+describe('Product Controller Create', () => {
+  it('should have a createProduct function', () => {
+    expect(typeof productController.createProduct).toBe('function');
   });
 });
