8332feb0eb7e8c34f11dfe1dee9505711ed22746
bearbearbear <bear-bear-bear@kakao.com>
2021-08-05 03:05:54 +0900

    feat: 'should call Product.create'테스트와 통과 코드 작성

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,7 +1,14 @@
 import { createProduct } from '../../controllers/products';
+import Product from "../../models/Product";
+
+Product.create = jest.fn();
 
 describe('Product Controller Create', () => {
   it('should have a createProduct function', () => {
     expect(typeof createProduct).toBe('function');
   });
+  it('should call Product.create', () => {
+    createProduct();
+    expect(Product.create).toBeCalled();
+  })
 });
