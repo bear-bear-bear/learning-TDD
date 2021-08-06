a9d745267ce421c89487a007a60227ed892e0520
bearbearbear <bear-bear-bear@kakao.com>
2021-08-06 17:55:50 +0900

    feat: create product 관련 테스트와 기능 작성

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,6 @@
 import Product from '../models/Product';
 
-export const createProduct = () => {
-  Product.create();
+export const createProduct = (req, res, next) => {
+  const createdProduct = Product.create(req.body);
+  res.status(201).json(createdProduct);
 };
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,14 +1,48 @@
+import { createRequest } from 'node-mocks-http/lib/mockRequest';
+import { createResponse } from 'node-mocks-http/lib/mockResponse';
+
 import { createProduct } from '../../controllers/products';
-import Product from "../../models/Product";
+import Product from '../../models/Product';
+import mockProduct from '../mock-data/product.json';
 
 Product.create = jest.fn();
 
+// 1 - 강의에서 안내해 준 것
+// let req, res, next;
+// beforeEach(() => {
+//   req = createRequest();
+//   res = createResponse();
+//   next = null;
+// });
+
+// 2 - 그냥 이렇게 하면 되는거 아닌가?
+const req = createRequest();
+const res = createResponse();
+const next = null;
+
 describe('Product Controller Create', () => {
+  beforeEach(() => {
+    req.body = mockProduct;
+  });
+
   it('should have a createProduct function', () => {
     expect(typeof createProduct).toBe('function');
   });
+
   it('should call Product.create', () => {
-    createProduct();
-    expect(Product.create).toBeCalled();
-  })
+    createProduct(req, res, next);
+    expect(Product.create).toBeCalledWith(mockProduct);
+  });
+
+  it('should return 201 response code', () => {
+    createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it('should return json body in response', () => {
+    Product.create.mockReturnValue(mockProduct);
+    createProduct(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(mockProduct);
+  });
 });
